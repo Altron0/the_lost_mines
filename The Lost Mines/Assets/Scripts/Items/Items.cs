79c350034515d9265ecdbf3e81e311959839531a
Your Name <you@example.com>
2025-02-15 11:49:12 +0300

    начало работы над инвентарем

---
+++ The Lost Mines/Assets/Scripts/Items/Items.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items : MonoBehaviour
+{
+    [SerializeField] ItemID id;
+    [SerializeField] int value;
+    [SerializeField] int exp;
+}
